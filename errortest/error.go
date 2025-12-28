9f2750ebdb5c3e655b2c0a0ba277da278b220232
JiaxiZhang <hdredbean123123123@outlook.com>
2025-12-28 11:48:12 +0800

    Error模块

---
+++ errortest/error.go
@@ -0,0 +1,9 @@
+package errortest
+
+import "errors"
+
+var (
+	NewErrorTest1 = errors.New("This is a New Error 1.")
+	NewErrorTest2 = errors.New("This is a New Error 2.")
+	NewErrorTest3 = errors.New("This is a New Error 3.")
+)
---
+++ errortest/test/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "Gogogo/errortest"
+  "fmt"
+
+)
+const (
+	errorTestConst1 = 1
+	errorTestConst2 = 1
+	errorTestConst3 = 1
+)
+
+func checkError(testData []int) error {
+	for i:=0; i < len(testData); i++{
+		if testData[i] == 1 {
+			return errortest.NewErrorTest1
+		}
+	}
+	return  nil
+}
+func main(){
+	testData1 := [3]int{errorTestConst1, errorTestConst2, errorTestConst3}
+	err := checkError(testData1[:])
+	fmt.Println(err)
+}
